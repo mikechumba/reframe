411c1d01bfc95e19e12222148b6c2e4c4e0086b8
Mike Chumba <michaelchumba09@gmail.com>
2023-02-20 22:52:49 +0300

    change: move local fonts initialization to app file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.scss'
+import "@/styles/globals.scss";
+import { Space_Grotesk, Poppins } from "@next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--ff-space-grotesk",
+});
+
+const poppins = Poppins({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+  variable: "--ff-poppins",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+          :root {
+            --ff-space-grotesk: ${spaceGrotesk.style.fontFamily};
+            --ff-poppins: ${poppins.style.fontFamily};
+          }
+        `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,7 @@
 import Head from 'next/head'
-import { Space_Grotesk, Poppins } from '@next/font/google'
 import NavBar from '@/layout/NavBar'
 import { getOrganizations } from '@/utils/dataService'
 import OrganizationList from '@/layout/OrganizationList'
-
-const spaceGrotesk = Space_Grotesk({
-  weight: ['400', '700'],
-  subsets: ["latin"],
-  variable: "--ff-space-grotesk",
-})
-
-const poppins = Poppins({
-  weight: ['300', '400', '500'],
-  subsets: ["latin"],
-  variable: "--ff-poppins",
-})
 
 export default function Home({ organizations }) {
   return (
@@ -24,12 +11,6 @@ export default function Home({ organizations }) {
         <meta name="description" content="Easily find organizations making an impact" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <style>{`
-          :root {
-            --ff-space-grotesk: ${spaceGrotesk.style.fontFamily};
-            --ff-poppins: ${poppins.style.fontFamily};
-          }
-        `}</style>
       </Head>
       <header className="banner">
         <NavBar />
