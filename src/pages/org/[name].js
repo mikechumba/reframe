1ceb76946e3ae7c77f15163d0cef5ec28cf3c07e
Mike Chumba <michaelchumba09@gmail.com>
2023-02-20 22:53:09 +0300

    add: organization profile page

---
+++ src/pages/org/[name].js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import NavBar from '@/layout/NavBar'
+import { getOrganizations } from '@/utils/dataService'
+import OrganizationList from '@/layout/OrganizationList'
+import { hiphenate } from '@/utils'
+
+export default function Home({ organizations }) {
+  return (
+    <>
+      <Head>
+        <title>Reframe</title>
+        <meta name="description" content="Easily find organizations making an impact" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="banner">
+        <NavBar />
+        <h1 className="visually-hidden">Reframe</h1>
+      </header>
+      <main className="main">
+        <OrganizationList organizations={organizations} />
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  const res = await getOrganizations();
+  const paths = res.organizations.map(({ name }) => ({ params: { name: hiphenate(name) } }))
+  console.log('::', paths)
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps() {
+  const res = await getOrganizations();
+
+  return { props: { organizations: res.organizations } }
+}
+
+
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,7 @@
 export const formatAmount = (amount) => {
   return `£${(Math.round(Number(amount || 0) * 100) / 100).toFixed(2)}`;
 }
+
+export const hiphenate = (str) => {
+  return str && str.toLowerCase().replace(/\s/g, '-');
+}
