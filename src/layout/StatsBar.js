21f5eda70fd52278f47773b95a376e0649693f14
Mike Chumba <michaelchumba09@gmail.com>
2023-02-21 17:51:24 +0300

    add: styling for stats section

--- src/layout/StatsBar.js
+++ src/layout/StatsBar.js
@@ -1,25 +1,25 @@
-import { formatAmount } from "@/utils";
+import { commarize, formatAmount } from "@/utils";
 
 const StatsBar = ({ org }) => {
   return (
     <section className="stats container">
-      <p className="stat-item">
-        <span>{formatAmount(org?.total_funds)}</span>
+      <p className="stats-item">
+        <span>{formatAmount(org?.total_funds)}</span><br />
         Raised
       </p>
       <hr />
-      <p className="stat-item">
-        <span>{formatAmount(org?.target_amount)}</span>
+      <p className="stats-item">
+        <span>{formatAmount(org?.target_amount)}</span><br />
         Annual Spend
       </p>
       <hr />
-      <p className="stat-item">
-        <span>{org?.annual_beneficiary_reach}</span>
+      <p className="stats-item">
+        <span>{commarize(org?.annual_beneficiary_reach)}</span><br />
         Annual Beneficiary Reach
       </p>
       <hr />
-      <p className="stat-item">
-        <span>30</span>
+      <p className="stats-item">
+        <span>{commarize(30)}</span><br />
         Donations
       </p>
     </section>
