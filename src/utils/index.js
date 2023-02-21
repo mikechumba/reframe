b4265dcafaeefc7f13247e63b8d060a51fb06519
Mike Chumba <michaelchumba09@gmail.com>
2023-02-21 17:13:03 +0300

    add: formatter to add commas to numbers

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,10 @@
 export const formatAmount = (amount) => {
-  return `£${(Math.round(Number(amount || 0) * 100) / 100).toFixed(2)}`;
+  const num = (Math.round(Number(amount || 0) * 100) / 100).toLocaleString('en-US', { minimumFractionDigits: 2 })
+  return `£${num}`;
+}
+
+export const commarize = (num) => {
+  return Number(num | 0).toLocaleString('en-US');
 }
 
 export const hiphenate = (str) => {
