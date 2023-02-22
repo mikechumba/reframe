981c097133744c48501d5f9b017cdaabe26f261b
Mike Chumba <michaelchumba09@gmail.com>
2023-02-22 11:17:15 +0300

    add: update font weight

--- src/layout/AboutSection.js
+++ src/layout/AboutSection.js
@@ -1,7 +1,7 @@
 const AboutSection = ({ org }) => {
   return <section className="container" aria-labelledby="section-title">
-    <h2 id="section-title">About <span className="visually-hidden">{ org?.name }</span></h2>
-    <p>{ org?.about }</p>
+    <h2 id="section-title" className="h2-small">About <span className="visually-hidden">{ org?.name }</span></h2>
+    <p className="mt-2">{ org?.about }</p>
   </section>
 }
 
