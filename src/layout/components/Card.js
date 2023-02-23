4b44dfa78345ad044df86b248ad7ee0b60d7777c
Mike Chumba <michaelchumba09@gmail.com>
2023-02-23 17:12:28 +0300

    add: gallery images for all orgs

--- src/layout/components/Card.js
+++ src/layout/components/Card.js
@@ -13,7 +13,7 @@ const Card = ({ org }) => {
     <div className="card" onClick={goToPage}>
       <div className="card-image">
         <img
-          src="https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          src={org.avatar}
           alt=""
         />
       </div>
